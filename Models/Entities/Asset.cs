51a68caf3a766c203956088a7c25bef4b72963ef
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-07-27 15:41:48 +0700

    Add Asset entity model

---
+++ Models/Entities/Asset.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhanMemCamDo.Models.Entities
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string ?AssetName { get; set; }
+        public string ?Description { get; set; }
+
+        // 👇 THÊM 2 DÒNG MỚI (Liên kết với Danh mục):
+        [Display(Name = "Loại Tài Sản")]
+        public int? AssetCategoryId { get; set; } // Cho phép null tạm thời để tránh lỗi dữ liệu cũ
+        public virtual AssetCategory? AssetCategory { get; set; }
+
+        // Liên kết 1-N Hợp đồng 
+        public virtual ICollection<PawnContract> ?PawnContracts { get; set; }
+    }
+}
