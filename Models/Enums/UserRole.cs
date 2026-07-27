7ca6903d6ed61de3725ee2693d729a99d95273c8
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-07-27 15:41:07 +0700

    Add UserRole enum

---
+++ Models/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace PhanMemCamDo.Models.Enums
+{
+    public enum UserRole
+    {
+        Admin = 0,    // Chủ cửa hàng (Full quyền)
+        Staff = 1     // Nhân viên (Chỉ được xem và tạo phiếu)
+    }
+}
