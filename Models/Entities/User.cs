706e02a7181e2af69a67a5171b35b58ed082a278
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-07-27 15:42:53 +0700

    Add User entity model

---
+++ Models/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PhanMemCamDo.Models.Enums;
+
+namespace PhanMemCamDo.Models.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ?Username { get; set; }
+
+        [Required]
+        //public string ?PasswordHash { get; set; } // Mật khẩu (đã mã hóa)
+        public string ?Password { get; set; } // Mật khẩu (chưa mã hóa) - Chỉ dùng tạm trong giai đoạn đầu
+        
+        [Required]
+        public string ?FullName { get; set; }
+
+        [Required]
+        public string ?Email { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string ?PhoneNumber { get; set; }
+
+        public UserRole Role { get; set; } = UserRole.Staff; // Mặc định là nhân viên
+        public bool IsActive { get; set; } = true; // Còn làm việc hay đã nghỉ
+    }
+}
