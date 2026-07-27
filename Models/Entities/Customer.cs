9c4c9516121e11df4d95de792e22b94db58334f4
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-07-27 15:41:32 +0700

    Add Customer entity model

---
+++ Models/Entities/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhanMemCamDo.Models.Entities
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [MaxLength(100)]
+        public string? FullName { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string? IdentityCard { get; set; }
+
+        [Phone]
+        [StringLength(15)]
+        public string? PhoneNumber { get; set; }
+
+        public string? Address { get; set; }
+
+        public List<PawnContract> PawnContracts { get; set; } = [];
+    }
+}
