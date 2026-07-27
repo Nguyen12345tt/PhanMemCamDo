6cc4c2680fafa2863b8d905c744c7d02d103984e
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-07-27 15:40:55 +0700

    Add CashFlowType enum

---
+++ Models/Enums/CashFlowType.cs
@@ -0,0 +1,8 @@
+﻿namespace PhanMemCamDo.Models.Enums
+{
+    public enum CashFlowType
+    {
+        Income = 0,  // Thu tiền vào (Khách đóng lãi, chuộc đồ)
+        Out = 1,  // Chi tiền ra (Đưa tiền cho khách cầm, trả tiền điện nước)
+    }
+}
