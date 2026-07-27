5b7a1601b0578d15c13c53f2250cd89c8b55bc6b
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-07-27 15:42:02 +0700

    Add AssetCategory entity model

---
+++ Models/Entities/AssetCategory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhanMemCamDo.Models.Entities
+{
+    // Bảng số 2 mới: Danh mục tài sản (Thay thế cho AssetImages)
+    public class AssetCategory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Tên Loại")]
+        public string ?Name { get; set; } // Ví dụ: Laptop, Xe máy, Điện thoại
+
+        // Liên kết ngược: Một loại có nhiều tài sản
+        public virtual ICollection<Asset> ?Assets { get; set; }
+    }
+}
