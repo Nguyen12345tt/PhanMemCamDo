3e4112a4e982029c3f8357552853bb57324c5f59
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-07-27 15:43:15 +0700

    Add ActionLog entity model

---
+++ Models/Entities/ActionLog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhanMemCamDo.Models.Entities
+{
+    public class ActionLog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ?ActionName { get; set; }   // VD: "XÓA HỢP ĐỒNG", "TẠO MỚI", "SỬA"
+        public string ?EntityName { get; set; }   // VD: "PawnContract", "Asset"
+        public string ?Description { get; set; }  // Chi tiết: "Xóa HĐ mã HD123..."
+        public DateTime Timestamp { get; set; } = DateTime.Now; // Thời gian thực hiện
+        public string UserName { get; set; } = "Admin"; // Tạm thời để cứng, sau này có Login thì lấy tên User thật
+    }
+}
